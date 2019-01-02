556f2d34943fcd9cc806ae81e81347f31b1e211f
los0731 <los0731@gmail.com>
2019-01-02 20:40:24 +0900

    styling input placeholder

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text')
+        
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': '제목 입력'}),
+            'text': forms.Textarea(
+                attrs={'placeholder': '내용 입력'}),
+        }
